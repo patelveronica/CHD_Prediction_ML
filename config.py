1fbb6cfcd93d00c296006d6bfccc22f54a74c238
Hsueh Chen <hsuehchen@Hsuehs-MacBook-Pro.local>
2021-11-18 07:47:38 -0800

    daniel_commit db integration

---
+++ config.py
@@ -0,0 +1,3 @@
+password = 'bootcamptSpot123?'
+database = 'chd_db'
+mongo_uri = f'mongodb+srv://Hsu_Che:{password}@chi-application.zvhzi.mongodb.net/{database}?retryWrites=true&w=majority'
--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-from flask import Flask, render_template, request
-#from flask_cors import cross_origin
+from flask import Flask, render_template, request, iedirect, url_for
+from flask_pymongo import PyMongo
+from config import mongo_uri
 import pickle
 import numpy as np
 
 app = Flask(__name__, template_folder= 'Template')
 model = pickle.load(open('model.pickle', 'rb'))
-
+app.config['MONGO'] = mongo_uri
+mongo = PyMongo(app)
+chd_user = mongo.db.chd_user_db
 
 @app.route('/')
 def home():
-    return render_template('index.html')    
+    saved_user = chd_user.find()
+    return render_template('index.html', saved_userr=saved_todos)
 
 @app.route('/predict',methods=['POST'])
 # @app.route('/predict')
@@ -19,8 +23,10 @@ def predict():
     # prediction = model.predict(final_features)
     # retrive values from Form
     init_features = [x for x in request.form.values()]
+    chd_user.insert_one({
+       init_features
+    })
     final_features = [np.array(init_features)]
-
     # make prediction
     prediction = model.predict(final_features)
 
