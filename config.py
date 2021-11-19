693ffeb08afd501dfa63073b93d0ff07499c91ad
Daniel Chen <hsueh.l.chen@outlook.com>
2021-11-18 16:23:47 -0800

    got home route to testing phase

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 password = 'bootcamptSpot123?'
 database = 'chd_db'
-mongo_uri = f'mongodb+srv://Hsu_Che:{password}@chi-application.zvhzi.mongodb.net/{database}?retryWrites=true&w=majority'
+username = 'Hsu_Che'
+mongo_uri = f'mongodb+srv://{username}:{password}@chi-application.zvhzi.mongodb.net/{database}?retryWrites=true&w=majority'
--- main.py
+++ main.py
@@ -6,14 +6,23 @@ import numpy as np
 
 app = Flask(__name__, template_folder= 'Template')
 model = pickle.load(open('model.pickle', 'rb'))
-app.config['MONGO'] = mongo_uri
+app.config['MONGO_URI'] = mongo_uri
 mongo = PyMongo(app)
 chd_user = mongo.db.chd_user_db
 
 @app.route('/')
 def home():
-    saved_user = chd_user.find()
-    return render_template('index.html', saved_userr=saved_todos)
+    prediction_information = mongo.db.chd_user_db.find_one()
+    if not prediction_information:
+        prediction_information = np.array([[0,20,0,0.0,0.0,0,1,0,110.0,110.0,70.0,22.0,70.0,100.0]])
+    else:
+        model_list = []
+        for information in prediction_information:
+            model_list.append(prediction_information[information])
+        final_features = [np.array(model_list)]
+        saved_user = model.predict(final_features)
+    
+    return render_template('index.html', prediction_text=saved_user)
 
 @app.route('/predict',methods=['POST'])
 # @app.route('/predict')
@@ -23,18 +32,16 @@ def predict():
     # prediction = model.predict(final_features)
     # retrive values from Form
     init_features = [x for x in request.form.values()]
-    column_list = ['gender', 'age', 'education', 'currentSmoker', 'cigsPerDay', 'BPMeds',
+    column_list = ['gender', 'age', 'currentSmoker', 'cigsPerDay', 'BPMeds',
        'prevalentStroke', 'prevalentHyp', 'diabetes', 'totChol', 'sysBP',
-       'diaBP', 'BMI', 'heartRate', 'glucose', 'TenYearCHD']
+       'diaBP', 'BMI', 'heartRate', 'glucose']
     for index, item in enumerate(init_features):
         userinput = {}
         userinput[column_list[index]] = item
     chd_user.insert_one({
        userinput
     })
-    final_features = [np.array(init_features)]
     # make prediction
-    prediction = model.predict(final_features)
 
     return redirect(url_for('index.html'))
 
